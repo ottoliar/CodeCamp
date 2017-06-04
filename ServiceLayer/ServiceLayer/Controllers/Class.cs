6b5b466b71354151cb33a02d089e1d3a051ca279
rottolia <rottolia@IRVLW71SPWBG2>
2017-06-03 21:03:31 -0700

    Add data access layer

---
+++ ServiceLayer/ServiceLayer/Controllers/Class.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceLayer.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Data = "Shawn Wildermuth", FavoriteColor = "Blue" });
+        }
+    }
+}
--- ServiceLayer/ServiceLayer/Startup.cs
+++ ServiceLayer/ServiceLayer/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,10 +15,10 @@ namespace ServiceLayer
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            _config = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
+        IConfigurationRoot _config;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -34,7 +30,7 @@ namespace ServiceLayer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddConsole(_config.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
